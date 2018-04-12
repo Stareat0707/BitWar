6e0452763d4d7cc6f45c2fb3e8be09609bb01e46
Stareat0707 <stareat0707@gmail.com>
2018-04-12 20:29:56 +0900

    Add GameController

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        StartCoroutine(MoveCamera());
+    }
+
+    IEnumerator MoveCamera()
+    {
+        while (true)
+        {
+            transform.position += new Vector3(speed, 0.0f, 0.0f);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject point;
+
+    public float minY, maxY;
+
+    public int startMoney;
+
+    public int nowMoney;
+
+    private float x;
+
+    public float height;
+
+    void Start()
+    {
+        x = 0.0f;
+        height = maxY - minY;
+        nowMoney = startMoney;
+        StartCoroutine(UpdateGraph());
+    }
+
+    IEnumerator UpdateGraph()
+    {
+        while (true)
+        {
+            Debug.Log(nowMoney);
+
+            Instantiate<GameObject>(point, new Vector3(x, nowMoney / (startMoney * 2.0f) * height + minY), new Quaternion());
+
+            x += 0.32f;
+
+            float percentage = Random.Range(-1.0f, 1.0f);
+
+            if ((nowMoney <= 0 && percentage < 0) || (nowMoney >= startMoney * 2 && percentage > 0))
+                percentage *= -1;
+
+            int additionalMoney = (int)((float)startMoney * percentage);
+
+            nowMoney = Mathf.Clamp(nowMoney + additionalMoney, 0, startMoney * 2);
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
